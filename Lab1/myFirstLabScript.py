f7ad3b581b14b51b987d53f5ac9200b06c00807d
tahaColak <tahayasir33033@gmail.com>
2025-02-20 14:44:00 +0300

    Implementing Question 1

---
+++ Lab1/myFirstLabScript.py
@@ -0,0 +1,10 @@
+'''
+@author:tahacolak
+
+'''
+#Question1
+stuName=input("Please enter your name: ")
+print(f"Hello {stuName} ")
+
+stuID=input("Please enter your Student ID: ")
+print(f"ID: {stuID}")
