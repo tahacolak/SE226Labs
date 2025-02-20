2538047fb9ddbb0cb3a147101009f7f28433ca07
tahacolak <89772910+tahacolak@users.noreply.github.com>
2025-02-20 15:38:05 +0300

    Create and Implement myFirstLabScript.cpp with all lab questions
    @author tahacolak

---
+++ Lab1/myFirstLabScript.cpp
@@ -0,0 +1,104 @@
+//Question1
+#include <iostream>
+#include <string>
+#include<stdio.h>
+using namespace std;
+
+int main() {
+   
+    string name;
+    string studentId;
+    
+    
+    cout << "What is your name? ";
+    std::getline(std::cin, name);
+    
+    
+    cout << "Hello " << name << "." << endl;
+    
+   
+    cout << "What is your Student ID? ";
+    getline(std::cin, studentId);
+    
+    
+    cout << "Your ID is " << studentId <<endl;
+    
+    return 0;
+}
+
+//Question2
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int main() {
+    double var1, var2;
+    
+    
+    cout << "Enter a value for var1: ";
+    cin >> var1;
+    
+   
+    cout << "Enter a value for var2: ";
+    cin >> var2;
+
+    double sum = var1 + var2;
+    double diff = var1 - var2;
+    double prod = var1 * var2;
+    
+    
+    cout << "var1 = " << var1 << endl;
+    cout << "var2 = " << var2 << endl;
+    cout << "sum = " << sum << endl;
+    cout << "diff = " << diff << endl;
+    cout << "prod = " << prod << endl;
+    
+    return 0;
+}
+
+//Question3
+#include <iostream>
+#include <string>
+#include <iomanip>  
+using namespace std;
+
+int main() {
+    string studentName;
+    double labGrade, midtermGrade, finalGrade, generalScore;
+    
+    cout << "Enter student name: ";
+    getline(cin, studentName);
+    
+    cout << "Enter lab grade: ";
+    cin >> labGrade;
+    
+    cout << "Enter midterm grade: ";
+    cin >> midtermGrade;
+    
+    cout << "Enter final grade: ";
+    cin >> finalGrade;
+    
+   
+    generalScore= (labGrade /4) + (midtermGrade *35/100) + (finalGrade *2/5);
+    
+    cout << fixed << setprecision(1);
+    cout << "Name: " << studentName << endl;
+    cout << "Lab: " << labGrade << endl;
+    cout << "Midterm: " << midtermGrade << endl;
+    cout << "Final: " << finalGrade << endl;
+    cout << "General Score: " << generalScore << endl;
+    
+    return 0;
+}
+//Question4
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+
+int main() {
+    cout<<("*\n**\n***\n**\n*");
+    
+    return 0;
+}
