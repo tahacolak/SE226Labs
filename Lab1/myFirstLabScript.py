b548a31918cbd73c502b769d8d3af18c27a7f432
tahaColak <tahayasir33033@gmail.com>
2025-02-20 14:55:06 +0300

    Update Lab1 with adding Q2

--- Lab1/myFirstLabScript.py
+++ Lab1/myFirstLabScript.py
@@ -8,3 +8,20 @@ print(f"Hello {stuName} ")
 
 stuID=input("Please enter your Student ID: ")
 print(f"ID: {stuID}")
+
+#Question2
+var1=float(input("First variable: "))
+var2=float(input("Second variable: "))
+
+sum=var1+var2
+diff=var1-var2
+prod=var1*var2
+
+if(diff<0):
+    print("Please takes difference reversedly!")
+
+else:
+    print(f"Difference = {diff}")
+
+print(f"{var1} + {var2}={ sum}")
+print(f"{var1} * {var2}= { prod}")
